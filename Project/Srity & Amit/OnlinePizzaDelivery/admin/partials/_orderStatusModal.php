4abd240818e453f3ed4b0c14bc0735625b64e5cb
M.R.Amit Hasan <amithasanmr@gmail.com>
2025-05-19 19:54:47 +0600

    i commit

---
+++ Project/Srity & Amit/OnlinePizzaDelivery/admin/partials/_orderStatusModal.php
@@ -0,0 +1,60 @@
+<?php 
+    $itemModalSql = "SELECT * FROM `orders`";
+    $itemModalResult = mysqli_query($conn, $itemModalSql);
+    while($itemModalRow = mysqli_fetch_assoc($itemModalResult)){
+        $orderid = $itemModalRow['orderId'];
+        $userid = $itemModalRow['userId'];
+        $orderStatus = $itemModalRow['orderStatus'];
+    
+?>
+
+<!-- Modal -->
+<div class="modal fade" id="orderStatus<?php echo $orderid; ?>" tabindex="-1" role="dialog" aria-labelledby="orderStatus<?php echo $orderid; ?>" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color: rgb(111 202 203);">
+        <h5 class="modal-title" id="orderStatus<?php echo $orderid; ?>">Order Status and Delivery Details</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="partials/_orderManage.php" method="post" style="border-bottom: 2px solid #dee2e6;">
+            <div class="text-left my-2">    
+                <b><label for="name">Order Status</label></b>
+                <div class="row mx-2">
+                <input class="form-control col-md-3" id="status" name="status" value="<?php echo $orderStatus; ?>" type="number" min="0" max="6" required>    
+                <button type="button" class="btn btn-secondary ml-1" data-container="body" data-toggle="popover" title="User Types" data-placement="bottom" data-html="true" data-content="0=Order Placed.<br> 1=Order Confirmed.<br> 2=Order Cancelled.">
+                    <i class="fas fa-info"></i>
+                </button>
+                </div>
+            </div>
+            <input type="hidden" id="orderId" name="orderId" value="<?php echo $orderid; ?>">
+            <button type="submit" class="btn btn-success mb-2" name="updateStatus">Update</button>
+        </form>
+        <!-- <?php 
+            $deliveryDetailSql = "SELECT * FROM `deliverydetails` WHERE `orderId`= $orderid";
+            $deliveryDetailResult = mysqli_query($conn, $deliveryDetailSql);
+            $deliveryDetailRow = mysqli_fetch_assoc($deliveryDetailResult);
+            $trackId = $deliveryDetailRow['id'];
+        ?> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+    }
+?>
+
+<style>
+    .popover {
+        top: -77px !important;
+    }
+</style>
+
+<script>
+    $(function () {
+        $('[data-toggle="popover"]').popover();
+    });
+</script>
